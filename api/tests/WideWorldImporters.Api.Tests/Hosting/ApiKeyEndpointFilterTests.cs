0b1d6851ef5b017cb5e74761b7ee05f19f782316
Dominic Sachs <DominicSachs@users.noreply.github.com>
2023-11-08 20:20:04 +0100

    feat: Exception middleware and health checks (#14)
    Co-authored-by: Dominic Sachs

---
+++ api/tests/WideWorldImporters.Api.Tests/Hosting/ApiKeyEndpointFilterTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using WideWorldImporters.Api.Hosting;
+
+namespace WideWorldImporters.Api.Tests.Hosting;
+
+public sealed class ApiKeyEndpointFilterTests
+{
+    private readonly ApiKeyEndpointFilter _testObject = new("the-key");
+
+    [Fact]
+    public async Task Returns_Unauthorized_If_Header_Can_Not_Be_Extracted()
+    {
+        var httpContext = new DefaultHttpContext();
+        var context = new DefaultEndpointFilterInvocationContext(httpContext);
+
+        var result = await _testObject.InvokeAsync(context, _ => ValueTask.FromResult<object?>(Results.Empty));
+
+        result.Should().BeOfType<UnauthorizedHttpResult>();
+    }
+
+    [Fact]
+    public async Task Returns_Unauthorized_If_Api_Key_Is_Invalid()
+    {
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers.Add("X-WWI-KEY", "the-invalid-key");
+        var context = new DefaultEndpointFilterInvocationContext(httpContext);
+
+        var result = await _testObject.InvokeAsync(context, _ => ValueTask.FromResult<object?>(Results.Empty));
+
+        result.Should().BeOfType<UnauthorizedHttpResult>();
+    }
+
+    [Fact]
+    public async Task Returns_Ok_If_Api_Key_Is_Valid()
+    {
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers.Add("X-WWI-KEY", "the-key");
+        var context = new DefaultEndpointFilterInvocationContext(httpContext);
+
+        var result = await _testObject.InvokeAsync(context, _ => ValueTask.FromResult<object?>(Results.Ok("Test-Ok")));
+
+        result.Should().BeOfType<Ok<string>>().Which.Value.Should().Be("Test-Ok");
+    }
+}
---
+++ api/tests/WideWorldImporters.Api.Tests/Hosting/ExceptionMiddlewareTests.cs
@@ -0,0 +1,64 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using WideWorldImporters.Api.Hosting;
+
+namespace WideWorldImporters.Api.Tests.Hosting;
+
+public sealed class ExceptionMiddlewareTests
+{
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    public ExceptionMiddlewareTests()
+    {
+        _logger = Substitute.For<ILogger<ExceptionMiddleware>>();
+    }
+
+    [Fact]
+    public async Task Returns_Internal_ServerError_And_Log_Exception()
+    {
+        var middleware = new ExceptionMiddleware(innerHttpContext => throw new Exception(), _logger);
+        var context = new DefaultHttpContext { Response = { Body = new MemoryStream() } };
+
+        await middleware.Invoke(context);
+
+        context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+        using (var reader = new StreamReader(context.Response.Body))
+        {
+            var streamText = await reader.ReadToEndAsync();
+
+            streamText.Should().BeEquivalentTo(string.Empty);
+            context.Response.StatusCode.Should().Be((int) HttpStatusCode.InternalServerError);
+
+            _logger.ReceivedWithAnyArgs().Log(default, default, default, default, default!);
+        }
+    }
+
+    [Fact]
+    public async Task Returns_Ok()
+    {
+        var middleware = new ExceptionMiddleware(innerHttpContext => Task.CompletedTask, _logger);
+        var context = new DefaultHttpContext();
+        var identity = new ClaimsIdentity(new[] { new Claim(JwtRegisteredClaimNames.Sub, "1") });
+        context.User = new ClaimsPrincipal(identity);
+
+        context.Response.Body = new MemoryStream();
+
+        await middleware.Invoke(context);
+
+        context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+        using (var reader = new StreamReader(context.Response.Body))
+        {
+            var streamText = await reader.ReadToEndAsync();
+
+            streamText.Should().BeEquivalentTo(string.Empty);
+            context.Response.StatusCode.Should().Be((int) HttpStatusCode.OK);
+
+            _logger.DidNotReceiveWithAnyArgs().Log(default, default, default, default, default!);
+        }
+    }
+}
