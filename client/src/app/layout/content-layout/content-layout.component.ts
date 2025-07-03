2d4e88c6e3f2b1fdc16721a7b000f0830006e52c
Dominic Sachs <dominic-sachs@gmx.de>
2025-07-03 19:07:59 +0200

    fix: Fixed unit tests

--- client/src/app/layout/content-layout/content-layout.component.ts
+++ client/src/app/layout/content-layout/content-layout.component.ts
@@ -1,9 +1,9 @@
-import { Component, signal, Signal } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, signal, Signal } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { WindowRef } from '../../shared/services/window.ref';
 import { AuthStatus } from '@app/auth/auth.models';
 import { AuthService } from '@app/auth/auth.service';
 import { NavigationMenuComponent } from '@app/layout/navigation-menu/navigation-menu.component';
@@ -18,12 +18,12 @@ export class ContentLayoutComponent {
   readonly isDarkMode = signal(false);
   readonly authStatus: Signal<AuthStatus>;
 
-  constructor(private readonly windowRef: WindowRef, private readonly authService: AuthService) {
+  constructor(private readonly authService: AuthService, @Inject(DOCUMENT) private readonly document: Document) {
     this.authStatus = this.authService.authStatus;
   }
 
   toggleDarkTheme(): void {
-    this.windowRef.nativeDocument.body.classList.toggle('dark-mode');
+    this.document.body.classList.toggle('dark-mode');
     this.isDarkMode.set(!this.isDarkMode());
   }
 }
